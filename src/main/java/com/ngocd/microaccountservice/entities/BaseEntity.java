145b0ccc72b84022533efa17d8d2b32bf9627eea
DoNgocDong <ngocdong2110.2003@gmail.com>
2025-01-16 16:27:35 +0700

    add -> config database auditing

---
+++ src/main/java/com/ngocd/microaccountservice/entities/BaseEntity.java
@@ -0,0 +1,34 @@
+package com.ngocd.microaccountservice.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+    @CreatedBy
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedBy
+    private LocalDateTime updatedAt;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    private String updatedBy;
+}
